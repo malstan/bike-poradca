b084ee2cd124e184066f5a67023d06c7ebfd0c66
Stanislav Malík <stanislavmalik@outlook.com>
2025-01-09 01:56:42 +0100

    ahhhh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  ssr: true,
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
-  plugins: [
-    { src: '~/plugins/gsap.client.js', mode: 'client' }
-  ]
 })
